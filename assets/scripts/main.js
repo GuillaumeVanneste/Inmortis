e29cfffda7a2ecad498dfe5ca3ddeba864b9a4f5
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2019-02-17 23:16:03 +0100

    Addig smooth scrolling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,23 +1,7 @@
 const $selection = document.querySelector(".selection")
 const $profileSelections = $selection.querySelectorAll(".profile-selection")
-
 const $profile = document.querySelector(".profile")
 const $profileCards = $profile.querySelectorAll(".profile-card")
-
-/* Loader */
-
-const $loader = document.querySelector('.loadingScreen')
-let loaded = false
-
-window.addEventListener('load', (event) =>
-{
-    console.log(event)
-    $loader.classList.add('loaded')
-    loaded = true
-})
-
-/* end loader */
-
 
 for (let i = 0; i < $profileSelections.length; i++) {
     const profile_selection = $profileSelections[i]
@@ -33,10 +17,34 @@ for (let i = 0; i < $profileSelections.length; i++) {
 }
 
 
+// SIDENAV
 $(document).ready(function(){
     $('.sidenav').sidenav();
 });
 
-const OS = navigator.platform;
 
-console.log(OS);
+// SMOOTH SCROLL
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
