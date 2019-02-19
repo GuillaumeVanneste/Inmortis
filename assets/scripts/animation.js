b16e7347f38fdb1ed4af93dd3306ce7249cad72c
GuillaumeVanneste <guillaumevanneste@hotmail.fr>
2019-02-19 11:55:22 +0100

    Add gifs & animations

--- assets/scripts/animation.js
+++ assets/scripts/animation.js
@@ -1,29 +1,32 @@
 jQuery(function($) {
 
+    var $animatables = $('.animatable');
+
     // Function which adds the 'animated' class to any '.animatable' in view
     var doAnimations = function() {
 
         // Calc current offset and get all animatables
-        var offset = $(window).scrollTop() + $(window).height(),
-            $animatables = $('.animatable');
+        var offset = $(window).scrollTop() + $(window).height();
+
 
         // Unbind scroll handler if we have no animatables
-        if ($animatables.length == 0) {
+        /*if ($animatables.length == 0) {
             $(window).off('scroll', doAnimations);
-        }
+        }*/
 
         // Check all animatables and animate them if necessary
         $animatables.each(function(i) {
             var $animatable = $(this);
             if (($animatable.offset().top - $animatable.height() + 20) < offset) {
                 $animatable.removeClass('animatable').addClass('animated');
+            } else if (($animatable.offset().top - $animatable.height() + 20) > offset) {
+                $animatable.removeClass('animated').addClass('animatable');
             }
         });
-
+        console.log(offset)
     };
 
     // Hook doAnimations on scroll, and trigger a scroll
     $(window).on('scroll', doAnimations);
-    $(window).trigger('scroll');
 
 });
